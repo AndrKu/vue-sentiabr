a1191feb743910d33491f4ca2892e5dd299e01ed
Andrey Kuznetsov <3057@mail.ru>
2022-09-15 20:18:39 +0500

    comm2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pwa: {
+    themeColor: '#8144E1',
+    manifestOptions: {
+      background_color: '#DF1818'
+    }
+  }
 })
